3f279ad126cf9ed5f89165d25bfdaff165c652a6
Mirents <mirent@mail.ru>
2022-07-11 22:13:23 +0300

    Add Readme.md

--- src/main/java/ru/mirents/jsonhelper/JsonHelperApp.java
+++ src/main/java/ru/mirents/jsonhelper/JsonHelperApp.java
@@ -3,6 +3,6 @@ package ru.mirents.jsonhelper;
 public class JsonHelperApp {
 
     public static void main(String[] args) {
-        JsonHelperWindow app = new JsonHelperWindow();
+        new JsonHelperWindow();
     }
 }
